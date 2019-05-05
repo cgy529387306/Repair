756c97181fde6adf6d5287a6b982f623297531b1
THINK <529387306@qq.com>
2019-05-06 00:00:36 +0800

    rrrr

--- app/src/main/java/com/yxw/cn/carpenterrepair/contast/UrlConstant.java
+++ app/src/main/java/com/yxw/cn/carpenterrepair/contast/UrlConstant.java
@@ -53,6 +53,8 @@ public class UrlConstant {
     public final static String ORDER_LIST = BASE_USER + "/ms60/api/fix/order/findAll";//订单条件查询
     public final static String ORDER_STATUS_LIST = BASE_USER + "/ms60/api/fix/order/getOrderStatusList";//getOrderStatusList
     public final static String ORDER_DETAIL = BASE_USER + "/ms60/api/fix/order/view/";//获取订单详情
+    public final static String ORDER_EXEPTION_APPOINT = BASE_USER + "/ms60/api/fix/order/exception/appointment";//预约异常
+    public final static String ORDER_EXEPTION_SIGN = BASE_USER + "/ms60/api/fix/order/exception/signIn";//签到异常
 
 
 
