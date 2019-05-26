293dc4d202bfb1584fe7d50b02eced7bfdb2df50
THINK <529387306@qq.com>
2019-05-26 21:04:39 +0800

    rrrr

---
+++ app/src/main/java/com/yxw/cn/carpenterrepair/activity/setting/AboutActivity.java
@@ -0,0 +1,34 @@
+package com.yxw.cn.carpenterrepair.activity.setting;
+
+import android.widget.TextView;
+
+import com.yxw.cn.carpenterrepair.BaseActivity;
+import com.yxw.cn.carpenterrepair.R;
+import com.yxw.cn.carpenterrepair.util.AppUtil;
+import com.yxw.cn.carpenterrepair.view.TitleBar;
+
+import butterknife.BindView;
+
+/**
+ * 关于
+ */
+public class AboutActivity extends BaseActivity {
+
+    @BindView(R.id.titlebar)
+    TitleBar titleBar;
+    @BindView(R.id.tv_version)
+    TextView tvVersion;
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.act_about;
+    }
+
+    @Override
+    public void initView() {
+        titleBar.setTitle("关于匠修");
+        tvVersion.setText(String.format("匠修 V%s", AppUtil.getVerName()));
+    }
+
+
+}
